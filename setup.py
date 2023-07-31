78ba26504887192b1233190e336edb2b1f779fe4
Reanna Panagides <reannapanagides@gmail.com>
2023-07-31 12:49:44 -0400

    updated files

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='bookpackage',
       version='0.1',
@@ -7,4 +7,4 @@ setup(name='bookpackage',
       author='Reanna Panagides',
       author_email='reannapanagides@gmail.com',
       license='MIT',
-      packages=['booklover', 'booklover_test'])
+      packages= find_packages())
