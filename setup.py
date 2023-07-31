cab53ba20d009d90db5c1acbf47ac4983f420d66
rpanagides <108998966+rpanagides@users.noreply.github.com>
2023-07-31 12:05:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='hw09',
+setup(name='bookpackage',
       version='0.1',
       description='book lover exercise',
       url='http://github.com/rpanagides/hw09',
       author='Reanna Panagides',
       author_email='reannapanagides@gmail.com',
       license='MIT',
-      packages=['hw09'])
+      packages=['bookpackage'])
